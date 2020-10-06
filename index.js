1100a02c0b12e005e0298011a0fb0513edd48d81
ember blooming <ember_b_moss@embers-MacBook-Air.local>
2020-10-06 11:23:51 +0200

    reviewed solution for first task

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function logOutNumbers() {
     for (let i = 1; i < 100; i++){
-        if (i % 3) {
+        if (i % 3 && i % 5 === 0) {
+       console.log("Jackpot!")
+        }else if(i % 3 === 0){
        console.log("This is divisible by 3")
-        }else if(i % 5){
-       console.log("This is divisible by 5")
-        }else if(i % 3 && i % 5) {
-        console.log("Jackpot!")
+        }else if (1 % 5 === 0) {
+        console.log("This is divisibe by 5")
         }else{
             console.log(i)
         }
